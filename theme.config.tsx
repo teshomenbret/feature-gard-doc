da0d2dcad4f34e7569c55873d2da3f19477d7e2d
teshomenbret <teshomenbret4@gmail.com>
2024-03-05 01:58:05 +0300

    add module and feature

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Feature Gard</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/teshomenbret/feature-gard',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/teshomenbret/feature-gard-doc',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Feature Gard Documentation || Being developed by Teshome Nbret',
   },
 }
 
